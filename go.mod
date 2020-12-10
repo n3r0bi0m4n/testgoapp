c8294d18a00c4689495391efd3862e29cbd2091c
Alexey Ermakov <n3r0bi0m4n@gmail.com>
2020-12-11 02:21:28 +0300

    initial commit :)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/n3r0bi0m4n/testgoapp
+
+go 1.15
+
+require github.com/n3r0bi0m4n/testgomod v1.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	testgomodule "github.com/n3r0bi0m4n/testgomod"
+)
+
+func main() {
+	testgomodule.Hi("Alexey")
+	testgomodule.Bye("Alexey")
+}
